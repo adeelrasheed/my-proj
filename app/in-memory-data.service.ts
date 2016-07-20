b3027520cbcec0fdb2d8fdff5780d4f146939ecd
Adeel Rasheed <adeel.rasheed@mitel.com>
2016-07-20 15:53:53 +0200

    Api implementation

---
+++ app/in-memory-data.service.ts
@@ -0,0 +1,14 @@
+export class InMemoryDataService {
+  createDb() {
+    let heroes = [
+      { id: 1, name: 'Daniyal' },
+      { id: 2, name: 'Adyan' },
+      { id: 3, name: 'Mustafa' },
+      { id: 4, name: 'Ibrahim' },
+      { id: 5, name: 'Waliallah' },
+      { id: 6, name: 'Zainab' },
+      { id: 7, name: 'Arfeen' }
+    ];
+    return {heroes};
+  }
+}
